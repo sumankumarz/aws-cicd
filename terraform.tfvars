dffb2bf8f468682b390b78da421bc55137d12cfa
Suman Kumar <suman@sumans-mbp.lan>
2021-11-29 22:11:32 +0100

    aws code pipeline and codebuild code

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+pipeline_deployment_bucket_name = "deployment-bucket"
+github_owner                    = "dgkanatsios"
+github_repo                     = "CKAD-exercises"
+github_oauth_key                = ""
+custom_tags = {
+  "environment" = "dev"
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "pipeline_deployment_bucket_name" {
+  description = "Bucket used by codepipeline and codebuild to store artifacts regarding the deployment"
+  type        = string
+}
+
+variable "custom_tags" {
+  description = "tags"
+  type        = map(string)
+}
+
+variable "github_owner" {
+  description = "GitHub owner name"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repo name"
+  type        = string
+}
